46af87e60b348bc8899229ec9d5163ffa2714942
PSHSRobotics <33577367+PSHSRobotics@users.noreply.github.com>
2019-03-06 06:52:19 -0600

    Create robot-config.h

---
+++ robot-config.h
@@ -0,0 +1,5 @@
+vex::brain Brain;
+vex::motor Spinner = vex::motor(vex::PORT1);
+vex::motor Spinner2 = vex::motor(vex::PORT2);
+vex::motor Claw = vex::motor(vex::PORT3);
+vex::controller c = vex::controller();
