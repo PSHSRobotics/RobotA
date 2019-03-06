646884d048db50f6001faaaeb85adfcda8751b43
PSHSRobotics <33577367+PSHSRobotics@users.noreply.github.com>
2019-03-06 06:59:31 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,7 @@ int main() {
         
         Spinner.spin(vex::directionType::fwd, c.Axis3.position(vex::percentUnits::pct) - c.Axis4.position(vex::percentUnits::pct), vex::percentUnits::pct);
         Spinner2.spin(vex::directionType::rev, c.Axis3.position(vex::percentUnits::pct) + c.Axis4.position(vex::percentUnits::pct), vex::percentUnits::pct);
-        if(c.ButtonR1.pressing()){
-            Claw.spin(vex::directionType::rev, 100, vex::percentUnits::pct);
-        }  
-        if(c.ButtonR2.pressing()){
-            Claw.spin(vex::directionType::fwd, 100, vex::percentUnits::pct);
-        }
+        Claw.spin(vex::directionType::fwd, c.Axis2.position(vex::percentUnits::pct), vex::percentUnits::pct);
 
         // Brain.Screen.print(c.Axis2.position(vex::percentUnits::pct));
         
